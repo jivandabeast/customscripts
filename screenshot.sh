0a1087d54e971ef80a9a52168572aebe8fcd45cb
jivan <jramjisingh@albany.edu>
2022-04-12 00:10:43 -0400

    change ss title format

--- screenshot.sh
+++ screenshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-firstHalf=$(date)
+firstHalf=$(date '+%Y-%m-%d %H:%M:%S')
 secondHalf=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
 fileName="$firstHalf-$secondHalf.png"
 fileLocation="/home/jivan/Pictures/sstemp/$fileName"
